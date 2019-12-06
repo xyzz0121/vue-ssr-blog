418144b3eff4a38c02faeff10c2f2d923b3d0dde
yangz <yangz@opera.com>
2019-12-06 17:50:07 +0800

    feat:连通数据库

--- blog-server/app.js
+++ blog-server/app.js
@@ -2,10 +2,15 @@
 const Koa = require('koa');
 const app = new Koa();
 const index = require('./routes/index');
+const {query} = require('./utils/query');
+const  sql = require('./utils/sql');
 
 app.use(index.routes(), index.allowedMethods());
 //将 public 目录设置为静态资源目录
 app.use(require('koa-static')(__dirname + '/public'));
+
+query(sql.QUERY_TABLE('users'));
+
 //将 views 设置为默认模板目录 TODO:koa-views安装失败
 // app.use(require('koa-views')(__dirname + '/views'));
 app.listen(3000);
--- blog-server/routes/index.js
+++ blog-server/routes/index.js
@@ -5,10 +5,13 @@ const fs = require('fs');
 const router = require('koa-router')()
 
 router.get('/index', async (ctx, next) => {
- ctx.type = 'text/html';
- ctx.body = fs.createReadStream('./views/index.html');
+    ctx.type = 'text/html';
+    ctx.body = fs.createReadStream('./views/index.html');
+});
+router.get('/api/list', async (ctx, next) => {
+    ctx.body = [{a:1}, {b:2}]
 });
 
- 
+
 
 module.exports = router
